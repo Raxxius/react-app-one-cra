91ca68a709ca7f7d7f4b0be4ad5882c5225b3cc5
Raxxius <raxxman@gmail.com>
2024-01-25 20:47:26 +0000

    it might work (probably not)

--- src/App.js
+++ src/App.js
@@ -4,6 +4,26 @@ import { useEffect, useState } from "react";
 function App() {
  const [colour, setColour] = useState("green")
 
+ useEffect(() => {
+  console.log(sessionStorage);
+  setColour(sessionStorage.currentColour);
+
+  // handleStorageChange function
+  const handleStorageChange = () => {
+    console.log("sessionStorage has changed")
+    setColour(sessionStorage.currentColour);
+  };
+
+  // Event listener
+  window.addEventListener("storage", handleStorageChange);
+  console.log("event listener engaged");
+
+  // Cleanup
+  return () => {
+    window.removeEventListener("storage", handleStorageChange);
+  };
+}, []);
+
   return (
     <div className="App" style={{backgroundColor : colour}}>
       <h1>React App1</h1>
